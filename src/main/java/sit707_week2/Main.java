da8846e2b262b26c10fac4f766dd4c67d9be7370
krishna chaudhari <87991666+kri2tle@users.noreply.github.com>
2024-03-27 15:17:21 +0530

    Update Main.java
    Removed drrible site and added office work site

--- src/main/java/sit707_week2/Main.java
+++ src/main/java/sit707_week2/Main.java
@@ -8,8 +8,7 @@ public class Main
 {
     public static void main( String[] args )
     {
-    	// SeleniumOperations.officeworks_registration_page("https://www.officeworks.com.au/app/identity/create-account");
-    	SeleniumOperations.dribbble_signup_page("https://dribbble.com/signup/new");
+    	SeleniumOperations.officeworks_registration_page("https://www.officeworks.com.au/app/identity/create-account");
     	SeleniumOperations.trustpilotinstitute_signup_page("https://trust.pilotinstitute.com/login/signup.php");
     }
 }
